069d0bc5534e3d98cc42a0ddc0fa0ea407a21219
Ssali Benjamin <benjamin.tamale@coseke.com>
2023-12-20 11:55:41 +0300

    Added some functions

--- functions.py
+++ functions.py
@@ -1,3 +1,22 @@
 """
 Functions for receiving modifying, reshaping and editing the data got from the HF servers.
 """
+import pandas as pd
+
+def fetchPastData() -> pd.DataFrame:
+    pass
+
+def filterData(data : pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def calcTools(data: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def displayTicks():
+    pass
+
+def someStrategy():
+    # Strategy algorithm
+    # open order
+    # close order
+    pass
